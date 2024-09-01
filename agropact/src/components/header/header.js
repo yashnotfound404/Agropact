280f034a6d1d32ed5b49cc57ad05355b36802c2d
Yash Mishra <yashmishra.del404@gmail.com>
2024-09-01 21:15:03 +0530

    Starting of header

--- agropact/src/components/header/header.js
+++ agropact/src/components/header/header.js
@@ -1,8 +1,31 @@
 import React from 'react';
-
+import '../header/header.css'
+import Logo from '../../assets/images/LOGO-SIH1.png'
 const Header =()=>{
     return(
-        
+        <>
+        <header>
+            <div className='top-container'>
+                <div className='row'> 
+                    <div className='col-sm-2'>
+                        <img src={Logo} />
+                    </div>
+
+                    {/* header search start here */}
+                    <div className='col-sm-5'>
+                        <div className='headerSearch d-flex align-item-center'>
+
+
+
+                        </div>
+                    </div>
+                    {/* header seatch end here  */}
+
+                </div>
+
+            </div>
+        </header>
+        </>
     )
 
 }
